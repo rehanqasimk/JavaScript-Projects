e7a44068b4a7f1674f54eb2a5fd7d64ec1bc4028
Rehan Qasim <qkrehan@gmail.com>
2022-02-26 17:52:46 +0500

    Project completed

--- 4- Exchange Rate/script.js
+++ 4- Exchange Rate/script.js
@@ -2,14 +2,42 @@ const currencyOne = document.getElementById("currency-one");
 const currencyTwo = document.getElementById("currency-two");
 const inputOne = document.getElementById("input-one");
 const inputTwo = document.getElementById("input-two");
+const exchangeDisplay = document.getElementById("exchangeDisplay");
 
 
-// 1- Add event Listener to the input field
-fetch("./package.json").then(
-    data => data.json()
-).then( obj => {
-    console.log(obj[0]);
-    document.write(`<h1>${obj[0].title}</h1>`);
-    document.write(`<p>${obj[0].body}</p>`);
-})
+// Swapping function
+const swapFunc = () => {
+    let temp = currencyOne.value;
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = temp;
+    exchangeRateCal();
+
+}
+
+//  Fetching Exhange Rate API
+const exchangeRateCal = () => {
+    fetch(`https://v6.exchangerate-api.com/v6/b9e62f2abda6e823b0c3a3cf/pair/${currencyOne.value}/${currencyTwo.value}`)
+        .then(data => data.json())
+        .then(json => {
+            console.log(typeof(+currencyTwo.value),typeof(json.conversion_rate));
+            const val =  Number(inputOne.value) * json.conversion_rate;
+            const display = `1 ${currencyOne.value} = ${json.conversion_rate} ${currencyTwo.value}`
+            inputTwo.value = val;
+            console.log(val);
+            exchangeDisplay.innerText = display;
+        }
+        )
+}
+
+
+
+currencyOne.addEventListener("change", exchangeRateCal);
+currencyTwo.addEventListener("change", exchangeRateCal);
+inputOne.addEventListener("change", exchangeRateCal);
+inputTwo.addEventListener("change", exchangeRateCal);
+swap.addEventListener("click", swapFunc);
+
+
+
+exchangeRateCal();
 
